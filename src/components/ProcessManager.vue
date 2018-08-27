b71b3778c254d5a4d902868e8670a4ae0bea5fcc
Sergio Liberman B <sliberman@alges.cl>
2018-08-27 21:45:32 +0200

    Process manager layout fixed

--- src/components/ProcessManager.vue
+++ src/components/ProcessManager.vue
@@ -1,31 +1,35 @@
 <template>
     <v-container>
-        <v-card>
-            <v-card-title>
-                <h1>Process manager</h1>
-            </v-card-title>
-            <v-card-text>
-                <p>This module allows the user to lunch or cancel the pipelines processes. It also shows the results.</p>
-            </v-card-text>
-            <v-card-text>
-                <v-select
-                        :items="loaded_pipelines_names"
-                        box
-                        label="Select pipeline to manage"
-                        @change="checkUserLoadedType"
-                ></v-select>
-            </v-card-text>
-        </v-card>
-        <v-flex xs12 sm10 offset-sm1 v-if="pipeline_selected !== ''">
-            <v-card>
-                <v-card-text>
-                    <v-layout>
-                        <h4 class="text-xs-left">{{ loaded_pipeline.name }} manager</h4>
-                    </v-layout>
-                </v-card-text>
-                <v-card-text>
-                    <v-card style="padding: 5px">
-                        <v-layout row wrap align-center>
+        <v-layout>
+            <v-flex xs12>
+                <v-card>
+                    <v-card-title>
+                        <h1>Process manager</h1>
+                    </v-card-title>
+                    <v-card-text>
+                        <p>This module allows the user to lunch or cancel the pipelines processes. It also shows the results.</p>
+                    </v-card-text>
+                    <v-card-text>
+                        <v-select
+                                :items="loaded_pipelines_names"
+                                box
+                                label="Select pipeline to manage"
+                                @change="checkUserLoadedType"
+                        ></v-select>
+                    </v-card-text>
+                </v-card>
+            </v-flex>
+        </v-layout>
+        <v-layout>
+            <v-flex xs12 sm10 offset-sm1 v-if="pipeline_selected !== ''">
+                <v-card>
+                    <v-card-text>
+                        <v-flex>
+                            <h4 class="text-xs-left">{{ loaded_pipeline.name }} manager</h4>
+                        </v-flex>
+                    </v-card-text>
+                    <v-card-text>
+                        <v-card style="padding: 5px">
                             <v-flex class="text-xs-center" v-if="process_state === 0">
                                 <v-btn fab color="blue" dark large><v-icon x-large circle
                                        @click="launch_pipeline">fa-play</v-icon></v-btn>
@@ -43,94 +47,88 @@
                                 <v-btn fab color="green" dark large><v-icon x-large circle>fa-check-double</v-icon></v-btn>
                                 <p>The pipeline has finished. Check its results below.</p>
                             </v-flex>
-                        </v-layout>
-                    </v-card>
-                    <v-flex><p></p></v-flex>
-                    <v-card style="padding: 5px">
-                        <v-layout>
+                        </v-card>
+                        <v-flex><p></p></v-flex>
+                        <v-card style="padding: 5px">
                             <v-list>
                                 <v-subheader>
                                     <h5>Logs</h5>
                                 </v-subheader>
-                                <v-flex xs12 lg5 mb-3>
-                                    <v-expansion-panel popout>
-                                        <v-expansion-panel-content
-                                                v-for="(item,i) in logs"
-                                                :key="i"
-                                        >
-                                            <div slot="header">{{ item.name }}</div>
-                                            <v-card>
-                                                <v-card-text>{{ item.content }}</v-card-text>
-                                            </v-card>
-                                        </v-expansion-panel-content>
-                                    </v-expansion-panel>
-                                </v-flex>
+                                <v-expansion-panel>
+                                    <v-expansion-panel-content
+                                            v-for="(item,i) in logs"
+                                            :key="i"
+                                            style="width: 100%"
+                                    >
+                                        <div slot="header">{{ item.name }}</div>
+                                        <v-card>
+                                            <v-card-text>{{ item.content }}</v-card-text>
+                                        </v-card>
+                                    </v-expansion-panel-content>
+                                </v-expansion-panel>
                             </v-list>
-                        </v-layout>
-                    </v-card>
-                    <v-flex><p></p></v-flex>
-                    <v-card style="padding: 5px">
-                        <v-layout>
+                        </v-card>
+                        <v-flex><p></p></v-flex>
+                        <v-card style="padding: 5px">
                             <v-list>
                                 <v-subheader>
                                     <h5>Results</h5>
                                 </v-subheader>
-                                <v-flex xs12 lg5 mb-3>
-                                    <v-expansion-panel popout>
-                                        <v-expansion-panel-content
-                                                v-for="(item,i) in results"
-                                                :key="i"
-                                        >
-                                            <div slot="header">{{ item.name }}</div>
-                                            <v-card>
-                                                <v-card-text v-if="!item.content.startsWith('data:image')">{{ item.content }}</v-card-text>
-                                                <v-card-text v-else><img :src="item.content" /></v-card-text>
-                                            </v-card>
-                                        </v-expansion-panel-content>
-                                    </v-expansion-panel>
-                                </v-flex>
+                                <v-expansion-panel>
+                                    <v-expansion-panel-content
+                                            v-for="(item,i) in results"
+                                            :key="i"
+                                    >
+                                        <div slot="header">{{ item.name }}</div>
+                                        <v-card>
+                                            <v-card-text v-if="!item.content.startsWith('data:image')">{{ item.content }}</v-card-text>
+                                            <v-card-text v-else><img :src="item.content" /></v-card-text>
+                                        </v-card>
+                                    </v-expansion-panel-content>
+                                </v-expansion-panel>
                             </v-list>
-                        </v-layout>
-                    </v-card>
-                    <v-flex><p></p></v-flex>
-                    <v-card style="padding: 5px">
-                        <v-layout>
+                        </v-card>
+                        <v-flex><p></p></v-flex>
+                        <v-card style="padding: 5px">
                             <h5 class="text-xs-left">Pipeline operations</h5>
-                        </v-layout>
-                        <v-layout columns>
                             <v-btn @click.stop="delete_dialog = true">Delete pipeline</v-btn>
                             <v-btn @click.native="duplicate_pipeline">Duplicate pipeline</v-btn>
                             <v-btn @click.stop="rename_dialog = true">Rename pipeline</v-btn>
-                        </v-layout>
-                    </v-card>
-                </v-card-text>
-            </v-card>
-        </v-flex>
-        <v-dialog width="500" v-model="delete_dialog">
-            <v-card>
-                <v-card-title class="headline">Confirmation</v-card-title>
-                <v-card-text>Are you sure you want to delete "{{ pipeline_selected }}"?</v-card-text>
-                <v-card-text>(This is a definitive decision)</v-card-text>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="green darken-1" flat @click.stop="delete_dialog = false">No</v-btn>
-                    <v-btn color="green darken-1" flat @click.native="delete_pipeline">Yes</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
+                        </v-card>
+                    </v-card-text>
+                </v-card>
+            </v-flex>
+        </v-layout>
 
-        <v-dialog width="500" v-model="rename_dialog">
-            <v-card>
-                <v-card-title class="headline">Rename</v-card-title>
-                <v-card-actions>
-                    <v-text-field
-                            label="Enter the new name for the pipeline"
-                            v-model="rename_new_name" required></v-text-field>
-                    <v-btn color="green darken-1" flat @click.stop="rename_dialog = false">Cancel</v-btn>
-                    <v-btn color="green darken-1" flat :disabled="rename_new_name.length === 0" @click.native="rename_pipeline">Ok</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
+        <v-layout>
+            <v-dialog width="500" v-model="delete_dialog">
+                <v-card>
+                    <v-card-title class="headline">Confirmation</v-card-title>
+                    <v-card-text>Are you sure you want to delete "{{ pipeline_selected }}"?</v-card-text>
+                    <v-card-text>(This is a definitive decision)</v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="green darken-1" flat @click.stop="delete_dialog = false">No</v-btn>
+                        <v-btn color="green darken-1" flat @click.native="delete_pipeline">Yes</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+        </v-layout>
+
+        <v-layout wrap row>
+            <v-dialog width="500" v-model="rename_dialog">
+                <v-card>
+                    <v-card-title class="headline">Rename</v-card-title>
+                    <v-card-actions>
+                        <v-text-field
+                                label="Enter the new name for the pipeline"
+                                v-model="rename_new_name" required></v-text-field>
+                        <v-btn color="green darken-1" flat @click.stop="rename_dialog = false">Cancel</v-btn>
+                        <v-btn color="green darken-1" flat :disabled="rename_new_name.length === 0" @click.native="rename_pipeline">Ok</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+        </v-layout>
     </v-container>
 </template>
 
@@ -202,16 +200,23 @@
         }).then((response) => {
           switch (response.data) {
             case 0:
+              // READY
               this.process_state = 0;
               break;
             case 2:
+              // FAILURE
               this.process_state = 2;
+              this.update_logs();
+              this.update_results();
               break;
             case 1:
+              // STARTED
               this.process_state = 1;
               break;
             case 3:
+              // SUCCESS
               this.process_state = 3;
+              this.update_results();
               break;
             default:
               this.process_state = 0;
@@ -273,24 +278,6 @@
           this.selected_pipeline_type = '';
         }
       },
-      load_pipelines_types() {
-        let url = api_url + "get/pipelines";
-        const options = {
-          method: 'GET',
-          headers: {
-            'Authorization': 'Token ' + localStorage.getItem("authorization_token")
-          },
-          xhrFields: {
-            withCredentials: true
-          },
-          url,
-        };
-        axios(options).catch(error => {
-          console.log(error);
-        }).then(request => {
-          this.pipelines_types = request.data;
-        });
-      },
       load_users_pipelines() {
         let url = api_url + "user_pipelines";
         const options = {
@@ -327,6 +314,7 @@
           console.log(error);
         }).then(() => {
           this.pipeline_selected = '';
+        }).then(() => {
           this.load_users_pipelines();
         });
       },
@@ -349,6 +337,7 @@
           console.log(error);
         }).then(() => {
           this.pipeline_selected = '';
+        }).then(() => {
           this.load_users_pipelines();
         });
       },
@@ -393,14 +382,15 @@
         };
         axios(options).catch(error => {
           console.log(error);
-        }).then(() => {
-        });
+        })
       }
     },
     watch: {
       loaded_pipeline: {
         deep: true,
         handler () {
+          this.update_logs();
+          this.update_results();
           if (this.loaded_pipeline === undefined) {
             return {
               name: ''
@@ -409,18 +399,32 @@
         }
       },
     },
-    created () {
-      this.load_pipelines_types();
-      this.load_users_pipelines();
-    },
     mounted() {
-      setInterval(() => {
-        if (this.pipeline_selected !== '') {
-          this.update_logs();
-          this.update_results();
-          this.update_button_state();
-        }
-      } , 2500);
+      let url = api_url + "get/pipelines";
+      const options = {
+        method: 'GET',
+        headers: {
+          'Authorization': 'Token ' + localStorage.getItem("authorization_token")
+        },
+        xhrFields: {
+          withCredentials: true
+        },
+        url,
+      };
+      axios(options).catch(error => {
+        console.log(error);
+      }).then(request => {
+        this.pipelines_types = request.data;
+      }).then(() => {
+        this.load_users_pipelines();
+      }).then(() => {
+        setInterval(() => {
+          if (this.pipeline_selected.length > 0 && this.process_state === 1) {
+            this.update_logs();
+            this.update_button_state();
+          }
+        } , 500);
+      });
     },
   };
 </script>
